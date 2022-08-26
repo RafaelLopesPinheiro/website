f61083b1293b42d3d5ca02d8d413c87fa77527c8
RafaelLopesPinheiro <rafaellopespinheiro@hotmail.com>
2022-08-26 11:08:26 -0300

    'cart app added '

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from products.models import Order
 # Create your models here.
 class Cart(models.Model):
     adrres = models.CharField(max_length=70)
+    orders = models.ManyToManyField(Order)
